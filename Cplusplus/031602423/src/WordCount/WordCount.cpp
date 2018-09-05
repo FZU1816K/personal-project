8f31a43765eaab06c496ccb74ad906e2b9272fcc
TragedyN <1020341630@qq.com>
2018-09-05 23:13:23 +0800

    统计文件的字符数

--- Cplusplus/031602423/src/WordCount/WordCount.cpp
+++ Cplusplus/031602423/src/WordCount/WordCount.cpp
@@ -6,17 +6,42 @@ int main(int argc,char* argv[])
 {
 	char* a = NULL;
 	a = argv[1];
-	//cout << a;
 	ifstream infile;
 	infile.open(a);   //将文件流对象与文件连接起来 
-	//assert(infile.is_open());   //若失败,则输出错误消息,并终止程序运行 
-
 	string s;
+	int count = 0;
+	/*
 	while (getline(infile, s))
 	{
 		cout << s << endl;
+		int i = 0;
+		while (1) {
+			if (s[i] != '\0') {
+				i++;
+				count++;
+			}
+			else {
+				break;
+			}
+		}
+		//cout << s[i];
+		if (s[i]=='\n') {
+			cout << "ok" << endl;
+			count++;
+		}
+	}
+	*/
+	char c;
+	infile >> noskipws;
+	while (!infile.eof())
+	{
+		infile >> c;
+		cout << c << endl;
+		count++;
 	}
 	infile.close();
+	count--;
+	cout << count;
 	
 	return 0;
 }
