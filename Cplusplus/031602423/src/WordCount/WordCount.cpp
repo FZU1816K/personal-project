306dcfbc634611def2df37e023e7737ac2d7d6d2
TragedyN <1020341630@qq.com>
2018-09-05 22:15:56 +0800

    读取.txt文件并输出内容

--- Cplusplus/031602423/src/WordCount/WordCount.cpp
+++ Cplusplus/031602423/src/WordCount/WordCount.cpp
@@ -1,8 +1,22 @@
 #include<iostream>
 #include<string>
+#include <fstream>
 using namespace std;
-int main()
+int main(int argc,char* argv[])
 {
-	printf("welcome");
+	char* a = NULL;
+	a = argv[1];
+	//cout << a;
+	ifstream infile;
+	infile.open(a);   //将文件流对象与文件连接起来 
+	//assert(infile.is_open());   //若失败,则输出错误消息,并终止程序运行 
+
+	string s;
+	while (getline(infile, s))
+	{
+		cout << s << endl;
+	}
+	infile.close();
+	
 	return 0;
 }
