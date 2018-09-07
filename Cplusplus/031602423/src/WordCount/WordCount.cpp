ed97cfd2b936170c5c0fa50767be5a4b3a019ac8
TragedyN <1020341630@qq.com>
2018-09-07 19:13:41 +0800

    统计行数，统计词频并排序

--- Cplusplus/031602423/src/WordCount/WordCount.cpp
+++ Cplusplus/031602423/src/WordCount/WordCount.cpp
@@ -3,15 +3,34 @@
 #include<map>
 #include <fstream>
 #include <algorithm>
+#include<vector>
 using namespace std;
 
+vector<pair<string, int>> tVector;
 char ff[1000];
+int lines=0;
 int character(char* path);
 int word(char strr[]);
 int isstring(char* c);
 int splitlen(char* f);
 void mytolower(char *s);
+void reorder(map <string, int> m);
+int cmp(const pair<string, int>& x, const pair<string, int>& y);
 
+int cmp(const pair<string, int>& x, const pair<string, int>& y)
+{
+	if(x.second != y.second)
+		return x.second > y.second;
+	else {
+		return x.first < y.first;//字典序靠前
+	}
+}
+void reorder(map <string, int> m) {
+	//map <string, int>::iterator m1_temp;
+	for (map<string, int>::iterator curr = m.begin(); curr != m.end(); curr++)
+		tVector.push_back(make_pair(curr->first, curr->second));
+	sort(tVector.begin(), tVector.end(), cmp);
+}
 void mytolower(char *s) {
 	int len = strlen(s);
 	for (int i = 0; i<len; i++) {
@@ -46,8 +65,10 @@ int character(char* path) {
 	while (!infile.eof())
 	{
 		infile >> c;
-		if (c == '\n')
+		if (c == '\n') {
+			lines++;
 			c = ' ';
+		}
 		ff[i] = c;
 		//cout << ff[i] << endl;
 		count++;
@@ -56,10 +77,11 @@ int character(char* path) {
 	ff[i] = '\0';
 	infile.close();
 	count--;
+	lines++;
 	return count;
 }
 int word(char strr[]) {
-	char* split = strtok(strr, ", ;:");
+	char* split = strtok(strr, ", ;:·~！@#￥%…&*（）-——+=[]{}\|""''?/");
 	int i = 0,count=0;
 	map <string, int> m1;
 	map <string, int>::iterator m1_Iter;
@@ -67,10 +89,10 @@ int word(char strr[]) {
 	while (split) {
 		//cout << "ok1";
 		if(splitlen(split)>=4 && isstring(split)==1){
-			count++;
 			cout <<"符合条件的字符串是：" <<split << endl;
 			if (m1.count(split) == 0)
 			{
+				count++;
 				m1.insert(pair <string, int>(split, 1));
 			}
 			else
@@ -80,19 +102,25 @@ int word(char strr[]) {
 		}
 		split = strtok(NULL, ", ;:");
 	}
+	/**/
+	reorder(m1);
+	/*
 	for (m1_Iter = m1.begin(); m1_Iter != m1.end(); m1_Iter++)
 	{
 		string key = m1_Iter->first;
 		int value = m1_Iter->second;
-		cout << "<" << key << ">" << ":" << value << endl;
+		//cout << "<" << key << ">" << ":" << value << endl;
 	}
+	*/
 	return count;
 }
+
+
 int main(int argc,char* argv[])
 {
 	char* a = NULL;
 	a = argv[1];
-	int characters,words,lines;
+	int characters,words;
 	characters = character(a);
 	//cout << ff<<endl;
 	mytolower(ff);
@@ -100,5 +128,12 @@ int main(int argc,char* argv[])
 	words = word(ff);
 	cout << "characters:" << characters << endl;
 	cout << "words:" << words << endl;
+	cout << "lines:" << lines << endl;
+	for (int i = 0; i<tVector.size(); i++)
+	{
+		if (i == 10)
+			break;
+		cout << "<" << tVector[i].first << ">" << ":" << tVector[i].second << endl;
+	}
 	return 0;
 }
