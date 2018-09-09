b13229d4606e147551940e34751c628bb27572ba
littlenorthwest <42861255+littlenorthwest@users.noreply.github.com>
2018-09-09 14:49:27 +0800

    Update main.cpp

--- Cplusplus/041602204/src/WordCount/WordCount/main.cpp
+++ Cplusplus/041602204/src/WordCount/WordCount/main.cpp
@@ -4,6 +4,12 @@
 #include"CountChar.h"
 #include"CountLines.h"
 using namespace std;
-int main()
+int main(int argc,char **argv)
 {
+	int temp=initialization(argc,argv);
+	if(temp==-1) {
+		cout<<"Sorry, something errors happened!"<<endl;
+		return 0;
+	}
+	
 }
