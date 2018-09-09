64e85059b94f0951b1088013cabee23d2cf71c70
pandaeathzr <031602215@fzu.edu.cn>
2018-09-10 00:19:58 +0800

    存在部分多一行的bug

--- Cplusplus/031602215/src/WordCount/WordCount/Countlines.h
+++ Cplusplus/031602215/src/WordCount/WordCount/Countlines.h
@@ -4,22 +4,28 @@
 using namespace std;
 
 
-int Countlines()
-{	//char *filename
+int Countlines(char *filename)
+{	//
 	ifstream infile;
-	infile.open("input.txt");
+	if (filename == nullptr)  //为了vs调试方便
+	{
+		infile.open("input.txt");
+	}
+	else infile.open(filename);
 	char c;
 	int row_count = 0,flag1=0,flag2=0;
 	infile >> noskipws;
 	while (!infile.eof())
 	{
 		infile >> c;  //按照字符输出
-		if (c != 10 && c!=' ' && infile.eof())
+		if (c != 10 && c!=' ')
 		{
 			flag1 = 1;
+			cout << int(c+1)<< endl;
 		}
 		if (c == 10)
 		{
+			cout << "1" << endl;
 			if (flag1 == 1)
 			{
 				flag1 = 0;
