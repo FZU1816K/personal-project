a84e99a89e11a27f67f41a489a7ccf4ca5c5af0a
littlenorthwest <42861255+littlenorthwest@users.noreply.github.com>
2018-09-09 14:58:14 +0800

    Update main.cpp

--- Cplusplus/041602204/src/WordCount/WordCount/main.cpp
+++ Cplusplus/041602204/src/WordCount/WordCount/main.cpp
@@ -7,9 +7,21 @@ using namespace std;
 int main(int argc,char **argv)
 {
 	int temp=initialization(argc,argv);
-	if(temp==-1) {
+	if(temp==-1) {                               //说明cmd窗口输入错误。 
 		cout<<"Sorry, something errors happened!"<<endl;
 		return 0;
 	}
+	//进行文件分析计算。 
+	chars_cnt=CountChar(argv[1]);
+	words_cnt=CountWords(argv[1]);
+	lines_cnt=CountLines(argv[1]);
+	top10=WordsTop10(argv[1]);
+	//
+	//输出结果。
+	cout<<"characters:"<<chars_cnt<<endl;
+	cout<<"words:"<<words_cnt<<endl;
+	cout<<"lines:"<<lines_cnt<<endl;
+	
+	return 0; 
 	
 }
