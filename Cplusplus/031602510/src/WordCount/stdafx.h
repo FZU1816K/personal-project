51ddbe630c25cc7950e31846f6f22ac37b37a6fb
葛家灿 <1175204449@qq.com>
2018-09-10 22:26:43 +0800

    v1.2

--- Cplusplus/031602510/src/WordCount/stdafx.h
+++ Cplusplus/031602510/src/WordCount/stdafx.h
@@ -8,5 +8,26 @@
 #include<string>
 #include<iterator>
 #include<cctype>
+#include<ctype.h>
 #include<unordered_map>
 #include<algorithm>
+
+using namespace std;
+
+class WordCount
+{
+
+private:
+	FILE * f;
+	const char *target_file;
+public:
+	WordCount(FILE *a, const char *b)
+	{
+		f = a;
+		target_file = b;
+	}
+	int WordNum();
+	int CharCount();
+	int LineCount();
+	void LetterCount();
+};
