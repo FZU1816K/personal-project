313b1c07a1828eae73a2c60bf589f218bd76ccdd
littlenorthwest <42861255+littlenorthwest@users.noreply.github.com>
2018-09-10 17:36:42 +0800

    Add files via upload

---
+++ Cplusplus/041602204/src/WordCount/WordCount/CountChar.cpp
@@ -0,0 +1,14 @@
+#include"CountChar.h"
+
+int CountChar(char * filename)
+{
+	int cnt = 0;
+	char c;
+	std::fstream file;
+	file.open(filename, std::ios::in);
+	while (file.get(c)) {
+		cnt++;
+	}
+	file.close();
+	return cnt;
+}
---
+++ Cplusplus/041602204/src/WordCount/WordCount/WordFrequency.h
@@ -0,0 +1,8 @@
+#pragma once
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<map>
+#include<vector>
+using namespace std;
+int WordsFrequency(char *filename);
