52388447117c3b0786ed09fda8ff23c9661b29f9
pandaeathzr <031602215@fzu.edu.cn>
2018-09-10 10:00:50 +0800

    解决了空白字符不能统计

--- Cplusplus/031602215/src/WordCount/WordCount/Countchar.h
+++ Cplusplus/031602215/src/WordCount/WordCount/Countchar.h
@@ -7,17 +7,19 @@ using namespace std;
 int CountChar(char *filename)
 {	//char *filename
 	ifstream infile;
+	
 	if (filename == nullptr)
 	{
 		infile.open("input.txt");
 	}
 	else infile.open(filename);
+	infile >> noskipws; //来强制读入每一个字符，不过滤空白符，包括换行符。
 	char c;
 	int count = 0;
 	while (!infile.eof())
 	{
 		infile >> c;  //按照字符输出
-		//cout << c << endl;
+		if (c == 10 && c == ' '&&c == '\t') cout << "11111111111111111111" << endl;
 		count++;
 	}
 	infile.close();
