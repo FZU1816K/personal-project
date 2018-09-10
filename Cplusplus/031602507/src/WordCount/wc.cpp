541d0247e7bc5282baf345913c600fd37b6da349
ProfessorChen <mulThree@163.com>
2018-09-10 23:16:23 +0800

    fix some bug and pass simple test

--- Cplusplus/031602507/src/WordCount/wc.cpp
+++ Cplusplus/031602507/src/WordCount/wc.cpp
@@ -17,7 +17,7 @@
 using namespace std;
 
 
-bool IsLetter(string::iterator it) {
+inline bool IsLetter(string::iterator it) {
 	if ((*it >= 65 && *it <= 90) || (*it >= 97 && *it <= 122)) {
 		return true;
 	}
@@ -26,7 +26,16 @@ bool IsLetter(string::iterator it) {
 
 	}
 }
-bool IsDigit(string::iterator it) {
+inline bool IsLetter(const char ch) {
+	if ((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122)) {
+		return true;
+	}
+	else {
+		return false;
+
+	}
+}
+inline bool IsDigit(string::iterator it) {
 	if (*it >= 48 && *it <= 57) {
 		return true;
 	}
@@ -37,18 +46,25 @@ bool IsDigit(string::iterator it) {
 
 
 
-int main(int argc, char *argv[]) {
+
+
+int main(int argc, char *argv[]) 
+{
 	int lineCount = 0;     //行数
 	int characterCount = 0;//字符数
 	int wordCount = 0;     //单词数
 	map<string, int> wordMap;
 	vector<string> lineBuffer;
+	string wordBuffer;
 	string tempStr;
+
+
+
 	if (argc == 2) {
 		//-------------------------------------------------------------------------
 		ifstream rf(argv[1],ios::in);
 		if (!rf) {
-			cout << "Open File Fail!1" << endl;
+			cout << "Open File Fail!" << endl;
 			//system("pause");
 			return 0;
 		}
@@ -83,51 +99,31 @@ int main(int argc, char *argv[]) {
 						flag = true;
 						lineCount++;
 					}
-					//如果这个字符是字母，判断是否为一个单词的开头
-					if (IsLetter(it)) {
-						string  tempWord;
-						int tempCharCount = 0;
-						for (string::iterator cursor = it; cursor != lineBuffer[i].end(); cursor++) {
-							//当字母未满4个且当前字符为字母时，继续判断下一个字符并将这个字符暂时保存
-							if (tempCharCount < 4 && IsLetter(cursor)) {
-								tempCharCount++;
-								tempWord += *cursor;
-							}
-							//当字母未满4个时且当前字符不是字母时，说明这个字符往前的字母都不是单词的一部分
-							else if (tempCharCount < 4 && !IsLetter(cursor)) {
-								it = cursor;
-								tempWord = "";
-								break;
-							}
-							//当字母满4个时且当前字符是字母或者数字时，说明这个字符往前都是单词的一部分（包括这个字符）
-							else if (tempCharCount >= 4 && (IsLetter(cursor) || IsDigit(cursor))) {
-								tempCharCount++;
-								tempWord += *cursor;
-							}
-							//当字母满4个时且当前字符不是字母也不是数字时，说明这个单词已经结束
-							else if (tempCharCount >= 4 && !IsLetter(cursor) && !IsDigit(cursor)) {
-								it = cursor;
-								transform(tempWord.begin(), tempWord.end(), tempWord.begin(), ::tolower); //转换为小写字母
-								pair<map<string, int>::iterator, bool> ret = wordMap.insert(make_pair(tempWord, 1));
-								if (!ret.second)
-									++ret.first->second;
-								tempWord = "";
-								wordCount++;
-								break;
-							}
-						}
-						//当一个单词处在行末时
-						if (tempWord.length() >= 4) {
-							transform(tempWord.begin(), tempWord.end(), tempWord.begin(), ::tolower); //转换为小写字母
-							pair<map<string, int>::iterator, bool> ret = wordMap.insert(make_pair(tempWord, 1));
-							if (!ret.second)
-								++ret.first->second;
-							tempWord = "";
-							wordCount++;
-							break;
-						}
+					if (IsLetter(it) || IsDigit(it)) {
+						wordBuffer += *it;
 					}
+					else {
+						if (wordBuffer.length() >= 4 && IsLetter(wordBuffer[0]) && IsLetter(wordBuffer[1]) && IsLetter(wordBuffer[2]) && IsLetter(wordBuffer[3])) {
+							transform(wordBuffer.begin(), wordBuffer.end(), wordBuffer.begin(), ::tolower); //转换为小写字母
+							pair<map<string, int>::iterator, bool> ret = wordMap.insert(make_pair(wordBuffer, 1));
+							if (!ret.second) {
+								++ret.first->second;
+							}
+							wordCount++;
+						}
+						wordBuffer = "";
+					}
+
 				}
+				if (wordBuffer.length() >= 4 && IsLetter(wordBuffer[0]) && IsLetter(wordBuffer[1]) && IsLetter(wordBuffer[2]) && IsLetter(wordBuffer[3])) {
+					transform(wordBuffer.begin(), wordBuffer.end(), wordBuffer.begin(), ::tolower); //转换为小写字母
+					pair<map<string, int>::iterator, bool> ret = wordMap.insert(make_pair(wordBuffer, 1));
+					if (!ret.second) {
+						++ret.first->second;
+					}
+					wordCount++;
+				}
+
 			}
 		}
 		//-------------------------------------------------------------------------
@@ -135,12 +131,12 @@ int main(int argc, char *argv[]) {
 
 		//-------------------------------------------------------------------------
 		ofstream wf("result.txt",ios::out);
-		wf<< "characters:" << characterCount <<endl << "words: "<< wordCount << endl << "lines:" << lineCount << endl;
+		wf << "characters:" << characterCount <<endl << "words: "<< wordCount << endl << "lines:" << lineCount << endl;
 		vector<pair<string,int> > wordVector;
 		for (map<string, int>::iterator it = wordMap.begin(); it != wordMap.end(); it++) {
 			wordVector.push_back(make_pair(it->first, it->second));
 		}
-		for (int i = 0;i != wordVector.size() && i < 10; i++) {
+		for (int i = 0;i != wordVector.size() ; i++) {
 			int maxFrequency = 0;
 			vector<pair<string, int> >::iterator maxFrequencyWord;
 			for (vector<pair<string, int> >::iterator it = wordVector.begin(); it != wordVector.end(); it++) {
@@ -153,7 +149,7 @@ int main(int argc, char *argv[]) {
 				wf << "<" << maxFrequencyWord->first << ">:" << maxFrequencyWord->second;
 			}
 			else {
-					wf << endl << "<" << maxFrequencyWord->first << ">:" << maxFrequencyWord->second;
+				wf << endl << "<" << maxFrequencyWord->first << ">:" << maxFrequencyWord->second;
 			}
 			maxFrequencyWord->second = -1;
 		}
