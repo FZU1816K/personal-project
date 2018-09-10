b0cf0367215e9c0139413124245c4e2d97e4489c
pandaeathzr <031602215@fzu.edu.cn>
2018-09-10 10:21:59 +0800

    删除调试行，注释改成英文

--- Cplusplus/031602215/src/WordCount/WordCount/Countchar.h
+++ Cplusplus/031602215/src/WordCount/WordCount/Countchar.h
@@ -13,13 +13,12 @@ int CountChar(char *filename)
 		infile.open("input.txt");
 	}
 	else infile.open(filename);
-	infile >> noskipws; //来强制读入每一个字符，不过滤空白符，包括换行符。
+	infile >> noskipws; //To force read in each character, do not filter whitespace, including line breaks
 	char c;
 	int count = 0;
 	while (!infile.eof())
 	{
-		infile >> c;  //按照字符输出
-		if (c == 10 && c == ' '&&c == '\t') cout << "11111111111111111111" << endl;
+		infile >> c;  //read character
 		count++;
 	}
 	infile.close();
