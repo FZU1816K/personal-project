3bf8c5509b5912b365b975266b6254e1ccda243a
Jeho <hawk_c@163.com>
2018-09-10 22:31:36 +0800

    WordCount1.0

---
+++ Cplusplus/031602513 /src/WordCount/wordc.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef WORDC_H
+#define WORDC_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Core {
+public:
+	int countCnum(string str);
+	int countWnum(string str);
+	void wordHigh(string str);
+	string txtTostr(string str);
+};
+
+
+#endif
