08db1d7d3273a44edd26736b43d2047c6167861e
pandaeathzr <031602215@fzu.edu.cn>
2018-09-10 23:30:33 +0800

    output.txt

--- Cplusplus/031602215/src/WordCount/WordCount/WordCount.cpp
+++ Cplusplus/031602215/src/WordCount/WordCount/WordCount.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+
 #include <cassert>
 #include <string>
 #include <map>
@@ -14,11 +15,19 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 
-	cout << "characters: "<<CountChar(argv[1]) << endl;   
-	cout << "words: " << Countwords_num(Countwords(argv[1])) << endl;
-	cout << "lines: " << Countlines(argv[1]) << endl;
-	Countwords_words(Countwords(argv[1]));
-	getchar();
+	ofstream fout("output.txt"); //output.txt
+	fout << "characters: "<<CountChar(argv[1]) << endl;   //no. of characters
+	fout << "words: " << Countwords_num(Countwords(argv[1])) << endl;  //no. of words
+	fout << "lines: " << Countlines(argv[1]) << endl;  //no. of lines
+	vector<pair<string, int>> tVector;
+	tVector =Countwords_words(Countwords(argv[1])); //words sorting
+	 //getchar();
+	for (int i = 0; i < tVector.size(); i++)
+	{
+		fout << tVector[i].first << ": " << tVector[i].second << endl;
+		if (i == 9) break;
+	}
+	fout.close();
 	
 	return 0;
 }
