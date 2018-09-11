1a89ccd1daca534439b551615d90af3d0ed5b5a0
comeony <747649138@qq.com>
2018-09-11 19:25:59 +0800

    the word frequency and top 10 word

---
+++ Cplusplus/031601131/src/WordCount/Word_Fre.cpp
@@ -0,0 +1,81 @@
+#include"Word_Fre.h"
+
+
+typedef pair<string, double> PAIR;
+struct CmpByValue {
+	bool operator()(const PAIR& lhs, const PAIR& rhs) {
+		return lhs.second > rhs.second;
+	}
+};
+
+int Word_Fre(char * filename)
+{
+	map<string, int> Word_Num_map;
+	char ch;
+	FILE *file;
+	fopen_s(&file, filename, "rt");
+	int flag = 0;			// state of DFA   in 0 1 2 3 4
+
+	string word;
+	for (; (ch = fgetc(file)) != EOF;)				//Determine the word and insert map
+	{
+		if ('A' <= ch && ch <= 'Z')
+			ch = ch + 32;
+		if (flag == 0) {
+			if (ch >= 'a'&&ch <= 'z') { flag = 1;	word = word + ch; }
+		}
+		else if (flag == 1) {
+			if (ch >= 'a'&&ch <= 'z') { flag = 2;	word = word + ch; }
+			else { flag = 0; word = ""; }
+		}
+		else if (flag == 2) {
+			if (ch >= 'a'&&ch <= 'z') { flag = 3;	word = word + ch; }
+			else { flag = 0; word = ""; }
+		}
+		else if (flag == 3) {
+			if (ch >= 'a'&&ch <= 'z') { flag = 4;	word = word + ch; }
+			else { flag = 0; word = ""; }
+		}
+		else if (flag == 4) {
+			if (ch >= 'a'&&ch <= 'z' || (ch >= '0'&&ch <= '9')) { word = word + ch; }
+			else {
+				Word_Num_map[word]++;
+			
+				word = "";
+				flag = 0;
+			
+			}
+		}
+	}
+
+	if (flag == 4) {
+		Word_Num_map[word]++;
+	
+	}
+	
+
+	vector <PAIR> Word_Num_vec(Word_Num_map.begin(), Word_Num_map.end());
+	sort(Word_Num_vec.begin(), Word_Num_vec.end(), CmpByValue());
+		/*
+		for (int i = 0; i != Word_Num_vec.size(); ++i) {
+			const char *ss = Word_Num_vec[i].first.c_str();
+			
+			cout << ss << ":" << Word_Num_vec[i].second << endl;
+		}
+		*/
+	ofstream outfile("result.txt", ios::out);
+	printf("The Top 10:\n:");
+	if(Word_Num_vec.size()<10)
+		for (int i = 0; i != Word_Num_vec.size(); ++i) {
+			const char *ss = Word_Num_vec[i].first.c_str();
+			cout << ss << ":" << Word_Num_vec[i].second << endl;
+			outfile << ss << ":" << Word_Num_vec[i].second << endl;
+		}
+	else
+		for (int i = 0; i != 10; ++i) {
+			const char *ss = Word_Num_vec[i].first.c_str();
+			cout << ss << ":" << Word_Num_vec[i].second << endl;
+			outfile << ss << ":" << Word_Num_vec[i].second << endl;
+		}
+	return 0;
+}
