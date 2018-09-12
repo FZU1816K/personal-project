9a7710393e94f123b0e5ee227431cc632e13553a
Zyym <3039262302@qq.com>
2018-09-12 20:17:19 +0800

    encapsulation function

---
+++ Cplusplus/031602147/src/WordCount/WordCount/CountCharacters.h
@@ -0,0 +1,13 @@
+#ifndef CountCharacters_H
+#define CountCharacters_H
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+#include<vector>
+#include<string>
+#include<string.h>
+#include<fstream>
+#include<algorithm>
+#include<windows.h>
+int CountCharacters(char *file);
+#endif
---
+++ Cplusplus/031602147/src/WordCount/WordCount/Word.h
@@ -0,0 +1,11 @@
+#include<string>
+using namespace std;
+#ifndef WORD_H
+#define WORD_H
+
+class Word{
+public:
+	string s;
+	int frequency;
+}; 
+#endif
