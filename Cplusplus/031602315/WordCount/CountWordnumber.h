b63413737f81c7092a2aa96c10c0a5c018dddbad
zimengxueying <32453573+zimengxueying@users.noreply.github.com>
2018-09-13 02:02:58 +0800

    Add files via upload

---
+++ Cplusplus/031602315/WordCount/CountWordnumber.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <fstream>     
+#include <iostream> 
+#include<string>
+#include <regex>
+#include<vector>
+#include<map>
+using namespace std;
+int CountWordnumber(string  mInputFileName);
+bool sortWord(pair<string, int> elem1, pair<string, int> elem2);
