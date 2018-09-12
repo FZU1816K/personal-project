fa7cafbfe7399569d2d38c6edcb48c1444c71555
Stellaaa18 <18120860780@163.com>
2018-09-12 22:33:26 +0800

    unit test

--- Cplusplus/181600215/src/UnitTest1/unittest1.cpp
+++ Cplusplus/181600215/src/UnitTest1/unittest1.cpp
@@ -25,6 +25,21 @@ namespace EmptyFile//测试空文件
 	};
 }
 
+namespace NotValidWords//没有有效单词
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			char filename[105] = "NotValidWords.txt";
+			int words = CountWords(filename);
+			Assert::IsTrue(words == 0);
+		}
+	};
+}
+
 namespace IncludeEmptyLines//包含空行
 {
 	TEST_CLASS(UnitTest1)
@@ -40,3 +55,47 @@ namespace IncludeEmptyLines//包含空行
 	};
 }
 
+namespace CharTest //含有空格制表符等空白字符时，能否正确统计字符数
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			char filename[105] = "CharTest.txt";
+			int characters = CountChar(filename);
+			Assert::IsTrue(characters == 149);
+		}
+	};
+}
+
+namespace WordsBeginWithNumber //测试单词以数字开头的情况
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			char filename[105] = "WordsBeginWithNumber.txt";
+			int characters = CountChar(filename);
+			Assert::IsTrue(characters == 0);
+		}
+	};
+}
+
+namespace LessThanFourLetters //测试单词小于四个字母的情况
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			char filename[105] = "LessThanFourLetters.txt";
+			int characters = CountChar(filename);
+			Assert::IsTrue(characters == 0);
+		}
+	};
+}
