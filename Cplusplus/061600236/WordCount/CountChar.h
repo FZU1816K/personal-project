789cade909d903420077c92f31b537bb41646b1f
yll7960 <911099620@qq.com>
2018-09-12 19:11:24 +0800

    update

---
+++ Cplusplus/061600236/WordCount/CountChar.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef _COUNTLINES_H_
+#define _COUNTLINES_H_  
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+#include<algorithm>
+#include<fstream>
+#include<string>
+#include<vector>
+#include<map>
+#include<string.h>
+#include<regex>
+
+using namespace std;
+
+int CountChar(char *filename);//Í³¼Æ×Ö·ûÊý 
+
+#endif
---
+++ Cplusplus/061600236/WordCount/CountWords.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef _COUNTLINES_H_
+#define _COUNTLINES_H_  
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+#include<algorithm>
+#include<fstream>
+#include<string>
+#include<vector>
+#include<map>
+#include<string.h>
+#include<regex>
+
+using namespace std;
+
+int CountWords(char *filename);//统计单词数 
+
+
+#endif
