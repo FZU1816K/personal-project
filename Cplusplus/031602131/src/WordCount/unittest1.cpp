90a0722f43e2e65f7716043fcaa04ba74c3cbdd5
Syxxx <303241829@qq.com>
2018-09-12 19:24:21 +0800

    main files

---
+++ Cplusplus/031602131/src/WordCount/unittest1.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../WordCount/WordCount.h"
+#include <iostream>
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+namespace UnitTest1
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: 在此输入测试代码
+			Cal a;
+			Assert::AreEqual<int>(30, a.CountChar());
+			Assert::AreEqual<int>(3, a.CountLine());
+		}
+
+	};
+}
