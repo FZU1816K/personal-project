f2695c499bf6d558bce473c3fc0337a808260950
TTTIDE <530217259@qq.com>
2018-09-16 09:20:40 +0800

    update6

--- Java/031602410/src/Main.java
+++ Java/031602410/src/Main.java
@@ -36,19 +36,21 @@ public class Main {
             characterscount+=readLine.length();//统计每行的字符数 最后只需再加上行数就是总字符数
             
             
-            for (String word : wordsArr1) {  
-                if(word.length() != 0){  //去除长度为0的行
+            for (String newword : wordsArr1) {  
+                if(newword.length() != 0){  //去除长度为0的行
                 	
-                	while(!(word.charAt(0)>=97&&word.charAt(0)<=122))
-                	{
-                		word = word.substring(1, word.length());
-                	}
-                	if((word.length()>=4)&&(Character.isLetter(word.charAt(0))&&Character.isLetter(word.charAt(1))&&Character.isLetter(word.charAt(2))&&Character.isLetter(word.charAt(3)))) 
+                	//while(!(word.charAt(0)>=97&&word.charAt(0)<=122)&&(word.length()>=4))//注意异常，substring用法
+                	//{
+                	//	word = word.substring(1);
+                	//}
+                	//重新阅读题目 ， 12345word中word不能算是单词 所以去掉这部分内容
+                	
+                	if((newword.length()>=4)&&(Character.isLetter(newword.charAt(0))&&Character.isLetter(newword.charAt(1))&&Character.isLetter(newword.charAt(2))&&Character.isLetter(newword.charAt(3)))) 
                 	
                 	{
                 		
                 	wordcount++;
-                    lists.add(word);  
+                    lists.add(newword);  
                 	}
                 }  
             }  
@@ -70,9 +72,9 @@ public class Main {
   
         }  
           
-        print.SortMap(wordsCount,wordline,wordcount,characterscount+wordline);    //排序并输出
- 
-      
+        print.SortMap(wordsCount,wordline,wordcount,characterscount+wordline-1);    //排序并输出
+        // 字符数为characterscount+wordline-1，每行几个字符加上n行-1，n-1个换行符
+
     }
 
 
