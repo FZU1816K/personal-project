a156e67dfa3c486aa6f9d61028e7811b45332132
waaaafool <38380730+waaaafool@users.noreply.github.com>
2018-09-16 22:30:05 +0800

    final

---
+++ 081600410/src/WordCount/main.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include"CharCount.h"
+#include"LineCount.h"
+#include"WordCount.h"
+#include"Frequency.h"
+
+
+using namespace std;
+
+
+int main(int argc, char** argv) {
+
+		
+		char s[100];
+
+		if (argv == NULL) {
+		cout << "请输入文件名" << endl;
+		return 0;
+		}
+		strcpy(s,argv[1]);
+		charCount(s);
+		frequency(s);
+		lineCount(s);
+		wordCount(s);
+	
+
+	return 0;
+}
