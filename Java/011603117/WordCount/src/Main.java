f364be6531084b2bf68e138d082a41f56dd9aeb7
TheHZDev <34000406+TheHZDev@users.noreply.github.com>
2018-09-16 17:50:53 +0800

    Update Main.java

--- Java/011603117/WordCount/src/Main.java
+++ Java/011603117/WordCount/src/Main.java
@@ -6,7 +6,7 @@ public  class Main
     public static void main(String[] args)
     {
         //开始读取字符
-        WordCountClass WordCount = new WordCountClass(args[0].substring(0,args[0].lastIndexOf("\\")),args[0].substring(arg[0].lastIndexOf("\\"));
+        WordCountClass WordCount = new WordCountClass(null,args[0]);
         WordCount.start();
     }
 
