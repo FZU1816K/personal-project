1dfbf343741e912b41398d29c9d704d4d2a8011f
syy0316026 <835448816@qq.com>
2018-09-16 10:42:16 +0800

    To be Standard

---
+++ Cplusplus/031602631/src/WordCount/Ccharacter.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include "Ccharacter.h"
+using namespace std;
+
+Character::Character() {}
+
+Character::Character(string file)
+{
+	finname = file;
+	number = 0;
+	foutname = "result.txt";
+}
+
+int Character::get()
+{
+	return  number;
+}
+void Character::count()
+{
+	ifstream fin;
+	fin.open(finname);
+	if (!fin.is_open())
+	{
+		cerr << "Could not open " << finname << endl;
+		fin.clear();
+	}
+	fin >> noskipws;
+	char c;
+	fin >> c;
+	while (fin.good())
+	{
+		if (c >= 0 && c <= 127)
+			++number;
+		fin >> c;
+	}
+	if (fin.eof()) {}
+	else if (fin.fail())
+		printf("Input terminated by data mismatch.\n");
+	else
+		printf("Input terminated for unknown reason.\n");
+	fin.close();
+}
+
+void Character::show()
+{
+	printf("characters: %d\n", number);
+}
+
+void Character::fwrite()
+{
+	ofstream fout;
+	fout.open(foutname, ios::out | ios::app);
+	if (!fout.is_open())
+	{
+		cerr << "Cant't open" << foutname << endl;
+		exit(EXIT_FAILURE);
+	}
+	fout << "characters: " << number << endl;
+	fout.close();
+}
---
+++ Cplusplus/031602631/src/WordCount/Clines.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include "Clines.h"
+using namespace std;
+
+Line::Line() {}
+
+Line::Line(string file)
+{
+	finname = file;
+	number = 0;
+	foutname = "result.txt";
+}
+
+int Line::get()
+{
+	return number;
+}
+
+void Line::count()
+{
+	ifstream fin;
+	if (finname == "")
+	{
+		printf("You must input file name first at initialization time.\n");
+		return;
+	}
+	fin.open(finname);
+	if (!fin.is_open())
+	{
+		cerr << "Could not open " << finname << endl;
+		fin.clear();
+	}
+	char buffer[1000];
+	while (fin.good())
+	{
+		fin.getline(buffer, 1000, '\n');
+		int i = strlen(buffer);
+		if (i != 0)
+			while (--i)
+			{
+				if (buffer[i] != ' '&& buffer[i] != '\n')
+				{
+					number++; break;
+				}
+			}
+	}
+	if (fin.eof()) {}
+	else if (fin.fail())
+		printf("Input terminated by data mismatch.\n");
+	else
+		printf("Input terminated for unknown reason.\n");
+	fin.close();
+}
+
+void Line::show()
+{
+	printf("lines: %d\n", number);
+}
+
+void Line::fwrite()
+{
+	ofstream fout;
+	fout.open(foutname, ios::out | ios::app); //×·¼Ó
+	if (!fout.is_open())
+	{
+		cerr << "Cant't open" << foutname << endl;
+		exit(EXIT_FAILURE);
+	}
+	fout << "lines: " << number << endl;
+	fout.close();
+}
