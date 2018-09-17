190717860d17d86c7fc14cbc76763f2499d92ed6
hym980417 <42564433+hym980417@users.noreply.github.com>
2018-09-17 10:24:55 +0800

    Add files via upload

---
+++ Cplusplus/041602209/src/wordcount/wordcount/CharacterCount.h
@@ -0,0 +1,11 @@
+
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int Character_Count(char * k);
---
+++ Cplusplus/041602209/src/wordcount/wordcount/LineCountcpp.cpp
@@ -0,0 +1,33 @@
+#include"LineCount.h"
+
+
+
+int Line_Count(char *k)
+{
+	char c;
+	fstream f(k, ios::in);
+	ofstream fout("result.txt", ios::app);
+	if (!f) {
+		fout << "Can't open file" << endl;
+
+		return -1;
+	}
+	int lineCnt = 0;
+
+	bool line_value = false;
+	while (f.get(c))
+	{
+		if (c > 32) line_value = true;
+
+		if (c == '\n'&&line_value == true)
+		{
+			lineCnt++;
+			line_value = false;
+		}
+	}
+	if (line_value == true)lineCnt++;//行数统计 
+	fout << "lines:" << lineCnt << endl;
+	f.close();
+	return lineCnt;
+
+}
