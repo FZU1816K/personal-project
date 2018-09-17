fdec3b8f0f2ab26d2b167933630444c3058566e2
hyh1998 <1072797231@qq.com>
2018-09-17 11:04:36 +0800

    Update WordCount.cpp

--- Cplusplus/031602113/src/WordCount/WordCount.cpp
+++ Cplusplus/031602113/src/WordCount/WordCount.cpp
@@ -1,40 +1,34 @@
-#include<fstream>  //ifstream
-#include<iostream>
-#include<map>
 #include<string>     
-#include<algorithm>
-#include <vector>
-#include"characters_count.h"
-#include"wordcount.h"
 #include"frequency.h"
 using namespace std;
 
 int main(int argc, char *argv[]) //int argc, char *argv[]
 {
-	/* ²ÎÊıµÄ¶¨Òå*/
+	/* å‚æ•°çš„å®šä¹‰*/
 
-	Characters c1;
-	Wordcount c2;
+	//Characters c1;
+	//Wordcount c2;
 	Frequency c3;
-	map<string, int> all_words;
-	pair<map<string, int>::iterator, bool> e_words;
+	//map<string, int> all_words;
+	//pair<map<string, int>::iterator, bool> e_words;
 	string word = "";
-	ifstream infile;
-	//string filename = "test.txt";
+	//ifstream infile;
+	//ofstream outfile;
+	//string filename = "cs1.txt";
+	//string t_outfile = "result.txt";
 	string filename = argv[1];
 
-	/**/
-
-	/*ĞÄ°®µÄ½Ó¿ÚÃÇ£¬ÔôÀÛ*/
-	c1.get_filename(filename);
-	c2.get_filename(filename);
+	//c1.get_filename(filename);
+	//c2.get_filename(filename);
 	c3.get_filename(filename);
-	c1.char_count();
-	c2.word_count();
-	c1.put_num();
-	c2.put_num();
+	//c1.char_count();
+	//c2.word_count();
+	//c1.put_num();
+	//c2.put_num();
 	c3.word_map();
-	c3.put_map();
+	c3.put_c();
+	//c3.put_words();
+	//c3.put_map();
 	/*  -///////////////-  */
 	system("pause");
 	return 0;
